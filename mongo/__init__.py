15ef668125356a0c86c5807adefe28b45e17ddaf
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-27 16:59:02 +0530

    Update __init__.py

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
+from config import MONGO_URI, MONGO_DB_NAME
 
-from config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-database = client.crocodile_game_bot
+try:
+    client = MongoClient(MONGO_URI)
+    database = client[MONGO_DB_NAME]
+except Exception as e:
+    print(f"Error connecting to database: {e}")
