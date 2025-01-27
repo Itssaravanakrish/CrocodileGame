4dd235c1c4340dc81797818c4b568bd74e86fa02
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-27 17:07:34 +0530

    Update env.py

--- config/env.py
+++ config/env.py
@@ -6,5 +6,6 @@ load_dotenv()
 
 BOT_TOKEN = getenv('BOT_TOKEN')
 MONGO_URI = getenv('MONGO_URI')
+MONGO_DB_NAME = getenv('MONGO_DB_NAME')
 SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
 PORT = getenv('PORT')
