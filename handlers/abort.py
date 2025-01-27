53c7f3753fdc3802034ba660957d02f289820588
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-27 16:49:14 +0530

    Update abort.py

--- handlers/abort.py
+++ handlers/abort.py
@@ -1,20 +1,18 @@
 from telegram import Update
-from telegram.ext import CallbackContext
-from telegram.ext import CommandHandler
-
+from telegram.ext import CallbackContext, CommandHandler
 from helpers.game import end_game
 from helpers.wrappers import admin_only
-
+import logging
 
 @admin_only
-def callback(update: Update, context: CallbackContext):
+def callback(update: Update, context: CallbackContext) -> None:
     try:
         end_game(context)
         update.effective_message.reply_text(
             f'{update.effective_user.mention_html()} aborted the game.',
         )
     except Exception as e:
+        logging.error(f'Error aborting game: {e}')
         update.effective_message.reply_text(f'Error: {e}')
-
 
 handler = CommandHandler('abort', callback)
