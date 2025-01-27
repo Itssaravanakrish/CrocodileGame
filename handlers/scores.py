32e66601c3fe76990affb3a29418958b8d8d6a90
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-27 16:53:05 +0530

    Update scores.py

--- handlers/scores.py
+++ handlers/scores.py
@@ -1,23 +1,24 @@
-from telegram import Update
-from telegram.ext import CommandHandler
 
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes
 from helpers.wrappers import nice_errors
 from mongo import users
 
-
 @nice_errors
-def callback(update: Update, _):
-    total_scores = users.total_scores(update.effective_user.id)
-    scores_in_chat = users.scores_in_chat(
-        update.effective_chat.id,
-        update.effective_user.id,
-    ) if (
-        update.effective_chat.type == 'supergroup'
-    ) else '<code>not in group</code>'
-
+def callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Handle the '/scores' command.
+    
+    Send the user's total scores and scores in the current chat.
+    """
+    user_id = update.effective_user.id
+    chat_id = update.effective_chat.id
+    
+    total_user_scores = users.total_scores(user_id)
+    scores_in_current_chat = users.scores_in_chat(chat_id, user_id) if update.effective_chat.type == 'supergroup' else '<code>not in group</code>'
+    
     update.effective_message.reply_text(
-        f'Your total scores: {total_scores}\nYour scores in this chat: {scores_in_chat}',
+        f'Your total scores: {total_user_scores}\nYour scores in this chat: {scores_in_current_chat}',
     )
-
 
 handler = CommandHandler('scores', callback)
