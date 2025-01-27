c01b33eeb13dc3b8d883d09b9230b9da3b84a8a4
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-27 16:47:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from telegram.ext import Defaults, Updater
-from config import BOT_TOKEN, PORT, SUDO_USERS
-import os, sys, asyncio
-from threading import Thread
 from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from config import BOT_TOKEN, PORT, SUDO_USERS
+import os
+import sys
+import asyncio
+from threading import Thread
 from handlers import add_handlers
 from helpers.filters import sudo_only
 from aiohttp import web
@@ -11,44 +12,34 @@ from aiohttp import web
 async def web_server():
     async def handle(request):
         return web.Response(text="Web server is running")
-
     app = web.Application()
     app.router.add_get('/', handle)
     return app
 
 async def main():
-    defaults = Defaults(
-        parse_mode='HTML',
-        disable_web_page_preview=True,
-        quote=False,
-        run_async=True,
-    )
-
-    updater = Updater(token=BOT_TOKEN, defaults=defaults)
-    dp = updater.dispatcher  # Use the dispatcher attribute of the Updater instance
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     if '-r' in sys.argv:
         for user in SUDO_USERS:
-            updater.bot.send_message(user, 'Restarted.')
+            application.bot.send_message(user, 'Restarted.')
 
-    def stop_and_restart(chat, msg):
-        updater.stop()
-        os.execl(sys.executable, sys.executable, *sys.argv, '-r', f'{chat}_{msg}', )
-
-    async def restart(update: Update, context: CallbackContext):
+    async def restart(update: Update, context: ContextTypes.DEFAULT_TYPE):
         update.effective_message.reply_text('Restarting...')
         Thread(target=stop_and_restart, args=(update.effective_chat.id, update.effective_message.message_id,)).start()
 
-    dp.add_handler(CommandHandler('r', restart, sudo_only))
-    add_handlers(dp)
+    def stop_and_restart(chat, msg):
+        application.stop()
+        os.execl(sys.executable, sys.executable, *sys.argv, '-r', f'{chat}_{msg}', )
+
+    application.add_handler(CommandHandler('r', restart, sudo_only))
+    add_handlers(application)
 
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
 
-    updater.start_polling(drop_pending_updates=True)
-    await asyncio.idle()
+    application.run_polling(drop_pending_updates=True)
 
 if __name__ == '__main__':
     asyncio.run(main())
