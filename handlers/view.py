765eead52a9258400a9d23fa43f3e5b0eb91c5fa
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-27 16:54:39 +0530

    Update view.py

--- handlers/view.py
+++ handlers/view.py
@@ -1,19 +1,21 @@
+
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import CallbackQueryHandler
-
 from helpers.game import get_game
 from helpers.wrappers import nice_errors
 
-
 @nice_errors
-def callback(update: Update, context: CallbackContext):
+def callback(update: Update, context: CallbackContext) -> None:
+    """
+    Handle the 'view' button press in a game.
+    
+    If the user is the host, send the game word as an alert. Otherwise, send a message indicating that the button is not for them.
+    """
     game = get_game(context)
-
     if game['host'].id == update.effective_user.id:
         update.callback_query.answer(game['word'], True)
     else:
         update.callback_query.answer('This is not for you.', True)
-
 
 handler = CallbackQueryHandler(callback, pattern='view')
